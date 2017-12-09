1c8af3b33e35b2c92418ceb7ead1eae9569670cc
elfiservice <elfiservice@hotmail.com>
2017-12-09 12:29:06 -0300

    feat: provide Grid and listener a click to paint

--- js/script.js
+++ js/script.js
@@ -47,6 +47,24 @@ var view = {
 
   render: function() {
     var Grid = octupus.getGrid();
+    var row = "";
+    for (var i = 0; i < Grid.height; i++) {
+      row += "<tr>";
+      for (var k = 0; k < Grid.width; k++) {
+        row += "<td class='cell"+ i + "0" + k +"'></td>";
+      }
+      row += "</tr>";
+    }
+    $("#pixel_canvas tr").remove();
+    $("#pixel_canvas").append(row);
+
+    for (var i = 0; i < Grid.height; i++) {
+      for (var k = 0; k < Grid.width; k++) {
+        $("#pixel_canvas tr td").on("click", function(cb) {
+          $("." + cb.target.className).css("background-color", Grid.color);
+        });
+      }
+    }
 
   }
 
