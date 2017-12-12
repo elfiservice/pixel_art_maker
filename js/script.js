04819f1816bd1be41c915b1b118ce87f8fd5b262
elfiservice <elfiservice@hotmail.com>
2017-12-12 18:24:52 -0300

    feat: change the color

--- js/script.js
+++ js/script.js
@@ -8,20 +8,26 @@ var model = {
 };
 
 var octupus = {
-  updateGrid: function(height, width, color) {
+  setGrid: function(height, width) {
     model.input_height = height;
     model.input_width = width;
-    model.colorPicker = color;
   },
 
   getGrid: function() {
-    var Grid = function (height, width, color) {
+    var Grid = function (height, width) {
       this.height = height;
       this.width = width;
-      this.color = color;
     }
 
-    return new Grid(model.input_height, model.input_width, model.colorPicker);
+    return new Grid(model.input_height, model.input_width);
+  },
+
+  setColor: function(color) {
+    model.colorPicker = color;
+  },
+
+  getColor: function() {
+    return model.colorPicker;
   },
 
   init: function() {
@@ -31,47 +37,52 @@ var octupus = {
 
 var view = {
   init: function() {
+    view.changeColor();
     $("#sizePicker").on("submit", function(){
       var input_height = $("#input_height").val();
       var input_width = $("#input_width").val();
-      var colorPicker = $("#colorPicker").val();
-      octupus.updateGrid(input_height, input_width, colorPicker);
+      octupus.setGrid(input_height, input_width);
 
       view.render();
 
       return false;
     });
 
+    $("#colorPicker").on("change", function() {
+      view.changeColor();
+    });
 
+  },
+
+  changeColor: function() {
+    var colorPicker = $("#colorPicker").val();
+    octupus.setColor(colorPicker);
   },
 
   render: function() {
     var Grid = octupus.getGrid();
-    var row = "";
+    var gridStructure = "";
     for (var i = 0; i < Grid.height; i++) {
-      row += "<tr>";
+      gridStructure += "<tr>";
       for (var k = 0; k < Grid.width; k++) {
-        row += "<td class='cell"+ i + "0" + k +"'></td>";
+        gridStructure += "<td class='cell"+ i + "0" + k +"'></td>";
       }
-      row += "</tr>";
+      gridStructure += "</tr>";
     }
     $("#pixel_canvas tr").remove();
-    $("#pixel_canvas").append(row);
+    $("#pixel_canvas").append(gridStructure);
 
     for (var i = 0; i < Grid.height; i++) {
       for (var k = 0; k < Grid.width; k++) {
         $("#pixel_canvas tr td").on("click", function(cb) {
-          $("." + cb.target.className).css("background-color", Grid.color);
+          $("." + cb.target.className).css("background-color", octupus.getColor());
         });
       }
     }
 
   }
-
 }
 
-
 octupus.init();
-
 
 });
